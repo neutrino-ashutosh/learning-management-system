b261edd38fb0857c6c73c8a2e145333e47bddcfd
neutrino-ashutosh <neutrino.ashutosh@gmail.com>
2024-10-27 17:45:54 +0530

    "cahnges"

--- build/utils/sendMail.js
+++ build/utils/sendMail.js
@@ -10,8 +10,9 @@ const path_1 = __importDefault(require("path"));
 const sendMail = async (options) => {
     const transporter = nodemailer_1.default.createTransport({
         host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '587'),
+        port: parseInt(process.env.SMTP_PORT || '465'),
         service: process.env.SMTP_SERVICE,
+        secure: true,
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD,
